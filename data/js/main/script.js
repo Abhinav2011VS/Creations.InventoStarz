a4a3eb7d0f9ef2fe03d8f901387853a7384fb136
AbhinavVS2011 <138882674+AbhinavVS2011@users.noreply.github.com>
2024-04-18 19:02:33 +0530

    Fix

--- data/js/main/script.js
+++ data/js/main/script.js
@@ -29,47 +29,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // JavaScript for Language Selection with auto-translation
-    const languageSelect = document.getElementById('language-select');
-    // Set the initial language selection based on stored value
-    const storedLanguage = localStorage.getItem('selectedLanguage');
-    if (storedLanguage) {
-        languageSelect.value = storedLanguage;
-        translateNavbar(storedLanguage); // Translate navbar on initial load
-    }
-    languageSelect.addEventListener('change', function () {
-        const language = this.value;
-        translateNavbar(language); // Translate navbar when language changes
-        // Store selected language in local storage
-        localStorage.setItem('selectedLanguage', language);
-        console.log('Selected language:', language);
-    });
-
-    function translateNavbar(language) {
-        console.log('Translating navbar to:', language);
-        const navbarTranslations = {
-            'Home': { 'en': 'Home', 'fr': 'Accueil', 'es': 'Inicio' },
-            'About': { 'en': 'About', 'fr': 'À Propos', 'es': 'Acerca De' },
-            'Mods': { 'en': 'Mods', 'fr': 'Mods', 'es': 'Modificaciones' },
-            'Data Packs': { 'en': 'Data Packs', 'fr': 'Packs De Données', 'es': 'Paquetes De Datos' },
-            'Texture/Resource Packs': { 'en': 'Texture/Resource Packs', 'fr': 'Packs De Textures', 'es': 'Paquetes De Texturas' },
-            'Shaders': { 'en': 'Shaders', 'fr': 'Shaders', 'es': 'Sombreadores' },
-            'Worlds': { 'en': 'Worlds', 'fr': 'Mondes', 'es': 'Mundos' },
-            'Addons': { 'en': 'Addons', 'fr': 'Modules Complémentaires', 'es': 'Complementos' },
-            'Bukkit Plugins': { 'en': 'Bukkit Plugins', 'fr': 'Plugins Bukkit', 'es': 'Complementos De Bukkit' },
-            'Customizations': { 'en': 'Customizations', 'fr': 'Personnalisations', 'es': 'Personalizaciones' },
-            'Mod Packs': { 'en': 'Mod Packs', 'fr': 'Packs De Modules', 'es': 'Paquetes De Modificaciones' }
-        };
-
-        Object.keys(navbarTranslations).forEach(item => {
-            const translation = navbarTranslations[item][language];
-            const navItem = document.querySelector('.nav-list li a[href="/' + item.toLowerCase().replace(/\s+/g, '-') + '/"]');
-            if (navItem) {
-                navItem.textContent = translation;
-            }
-        });
-    }
-
     // JavaScript for handling modal popup
     const settingsBtn = document.getElementById('settings-btn');
     const closePopupBtns = document.querySelectorAll('.popup-content .close');
