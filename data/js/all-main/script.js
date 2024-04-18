ac15604524add375a8ca4bc7f845bed64eeb47d7
AbhinavVS2011 <138882674+AbhinavVS2011@users.noreply.github.com>
2024-04-18 18:28:38 +0530

    Fix

--- data/js/all-main/script.js
+++ data/js/all-main/script.js
@@ -23,13 +23,21 @@ document.addEventListener('DOMContentLoaded', function () {
 // Disable right-click context menu
 document.addEventListener("contextmenu", function (e) {
     e.preventDefault();
-    alert("Right-click is disabled.");
+    showPopup("Right Click is disabled");
 });
 
 // Disable Ctrl+U, F12, and Fn+F12
 document.addEventListener("keydown", function (e) {
     if ((e.ctrlKey && e.key === "u") || e.key === "F12" || (e.key === "F12" && e.key === "Control")) {
         e.preventDefault();
-        alert("Viewing source or inspection is not allowed.");
+        showPopup("Inspection or viewing source is disabled.");
     }
 });
+
+// Function to show the pop-up
+function showPopup(message) {
+    var popupContainer = document.getElementById("popup-container");
+    var popupMessage = document.getElementById("popup-message");
+    popupMessage.textContent = message;
+    popupContainer.style.display = "block";
+}
