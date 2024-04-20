ef2eed7eabd075e486f402f861e9cdeddf1e952c
AbhinavVS2011 <138882674+AbhinavVS2011@users.noreply.github.com>
2024-04-20 21:49:30 +0530

    Fix

--- data/js/all-main/script.js
+++ data/js/all-main/script.js
@@ -49,3 +49,60 @@ function showPopup(message) {
         }, 1000);
     }, 2000);
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Check if dark mode is enabled in local storage
+    const isDarkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+
+    // Update stylesheet based on dark mode state
+    const stylesheetLink = document.getElementById('stylesheet');
+    if (isDarkModeEnabled) {
+        stylesheetLink.href = '/data/css/all-main/dark/styles.css';
+    } else {
+        stylesheetLink.href = '/data/css/all-main/light/styles.css';
+    }
+
+    // Event listener for dark mode toggle
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    darkModeToggle.checked = isDarkModeEnabled; // Set the initial state of the toggle
+    darkModeToggle.addEventListener('change', function () {
+        if (this.checked) {
+            document.body.classList.add('dark-mode');
+            // Store dark mode state in local storage
+            localStorage.setItem('darkModeEnabled', 'true');
+            // Update stylesheet link
+            stylesheetLink.href = '/data/css/all-main/dark/styles.css';
+        } else {
+            document.body.classList.remove('dark-mode');
+            // Store dark mode state in local storage
+            localStorage.setItem('darkModeEnabled', 'false');
+            // Update stylesheet link
+            stylesheetLink.href = '/data/css/all-main/light/styles.css';
+        }
+    });
+
+    // JavaScript for handling modal popup
+    const settingsBtn = document.getElementById('settings-btn');
+    const closePopupBtns = document.querySelectorAll('.popup-content .close');
+    const modals = document.querySelectorAll('.popup');
+
+    settingsBtn.addEventListener('click', function () {
+        document.getElementById('settings-modal').style.display = 'block';
+    });
+
+    closePopupBtns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            modals.forEach(modal => {
+                modal.style.display = 'none';
+            });
+        });
+    });
+
+    window.addEventListener('click', function (event) {
+        modals.forEach(modal => {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+    });
+});
